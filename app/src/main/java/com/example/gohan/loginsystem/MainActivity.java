19baed568b4da5f25e3eefa05ee2b19d4122a10d
dhjasnani <asnani.dheeraj@yahoo.com>
2016-06-17 01:51:58 +0530

    Entry Fields Created

--- app/src/main/java/com/example/gohan/loginsystem/MainActivity.java
+++ app/src/main/java/com/example/gohan/loginsystem/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.gohan.loginsystem;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView Login,gotoRegister,Register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        gotoRegister = (TextView) findViewById(R.id.reg_button);
+
+    }
+    public void gotoReg(View view)
+    {
+        Intent intent = new Intent(this,Register_Activity.class);
+        startActivity(intent);
     }
 }
